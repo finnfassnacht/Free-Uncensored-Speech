3de052e2a1c4fadab3d538315074e82ec797ab32
Finn-jf <finnfassnacht@gmail.com>
2022-12-23 13:12:39 +0100

    Update index.js

--- www/post/index.js
+++ www/post/index.js
@@ -13,7 +13,7 @@ image_input.addEventListener("change", function() {
     var image = document.getElementById("image_url").value;
     var img_el = document.createElement("img")
     img_el.src = image;
-    img_el.style = "width: 83%;height: 350px;border-radius: 5px;border: 1px solid black;"
+    img_el.style = "width: 70%;height: 350px;border-radius: 5px;border: 1px solid black;"
     document.getElementById("mainbox").appendChild(img_el);
 });
 
